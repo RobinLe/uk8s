31866de3250c66b72bcea834cd4870afc8e2642b
robinle <miaomiao3312763@qq.com>
2017-05-11 17:58:15 +0800

    add ui

--- registry/urls.py
+++ registry/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.catalog, name='catalog'),
-    url(r'^(?P<name>[a-z0-9_-]+)/$', views.registry, name='registry'),
+    url(r'^$', views.index, name='index'),
+    url(r'^registry/$', views.registry, name='registry'),
+    url(r'^registry/(?P<name>[a-z0-9_-]+)/$', views.images, name='images'),
 ]
--- registry/views.py
+++ registry/views.py
@@ -8,34 +8,62 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+REGISTRY_BASE = "https://192.168.3.110:5000/v2"
 
-def req(url):
+def req(url, headers=None):
     """parser response of request to json format
     """
-    response = requests.get(url, verify=False)
+    if headers is None:
+        headers = {}
+    response = requests.get(url, verify=False, headers=headers)
     if response.status_code == 200:
         response = json.loads(response.text)
         return response
     return None
 
-
-def catalog(request):
-    """get registry catalog
+def get_registries():
+    """get docker registry list
     """
-    url = "https://192.168.3.110:5000/v2/_catalog"
+    url = "/".join([REGISTRY_BASE, "_catalog"])
     response = req(url)
     if response is not None:
-        context = {'catalog': response["repositories"]}
-        return render(request, 'catalog.html', context)
-    return render(request, 'catalog.html', [])
+        return response["repositories"]
+    return []
 
-
-def registry(request, name):
-    """get tag list
+def get_images(name):
+    """get all images of the registry
     """
-    url = "https://192.168.3.110:5000/v2/" + name + "/tags/list"
+    url = "/".join([REGISTRY_BASE, name, "/tags/list"])
     response = req(url)
+    image_list = []
     if response is not None:
-        context = {"catalog": response["tags"]}
-        return render(request, 'catalog.html', context)
-    return render(request, 'catalog.html', [])
+        headers = {"Accept": "application/vnd.docker.distribution.manifest.v2+json"}
+        tags = response["tags"]
+        for tag in tags:
+            url = "/".join([REGISTRY_BASE, name, "/manifests", tag])
+            response = req(url, headers)
+            if response is not None:
+                image = {}
+                image["size"] = response["config"]["size"]
+                for i in response["layers"]:
+                    image["size"] += i["size"]
+                image["size"] = round(float(image["size"]) / 1024 / 1024, 2)
+                image["id"] = response["config"]["digest"]
+                image["tag"] = tag
+                image_list.append(image)
+    return image_list
+
+def registry(request):
+    """render docker registry list page
+    """
+    return render(request, 'registry.html', {"registries": get_registries()})
+
+def images(request, name):
+    """render docker images list page
+    """
+    return render(request, 'images.html', {"images": get_images(name)})
+
+def index(request):
+    """render index page
+    """
+    return render(request, 'index.html')
